de0c58bd921613c07b0d9e305def56411e0b6dc1
vividandrew <andrew.warnock@outlook.com>
2024-05-29 17:07:56 +0100

    added correct theme to admin sections Added correct theme to the admin sections

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,7 +6,7 @@
     </p>
     @if(Auth::user()->role != "Warehouse")
     <div class="grid grid-cols-2">
-        <div class="bg-gray-400 rounded fixed p-5">
+        <div class="bg-orange-700 rounded fixed p-5">
             @include('admin.EOD')
             <a class="text-blue-300 hover:text-blue-400" href="{{route('admin.print.eod')}}">Print EOD</a>
         </div>
--- resources/views/shared/admin-template.blade.php
+++ resources/views/shared/admin-template.blade.php
@@ -8,7 +8,7 @@
     @vite('resources/js/app.js')
     @vite('resource/')
 </head>
-<body class="bg-gray-600">
+<body class="bg-red-900 text-white font-bold">
 <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
     <span class="sr-only">Open sidebar</span>
     <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -17,7 +17,7 @@
 </button>
 
 <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-    <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+    <div class="h-full px-3 py-4 overflow-y-auto bg-red-600 text-white font-bold">
         <ul class="space-y-2 font-medium">
             <li>
                 <a href="{{route('admin.index')}}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
